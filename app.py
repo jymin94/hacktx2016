ee40534f4f6840004356362def1324c178d8a84d
derek949 <derek.truong@utexas.edu>
2016-10-22 16:04:00 -0500

    rdy to test pyrebase

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from firebase import firebase
-from flask import * 
+from flask import *
+import pyrebase
+
+config = {
+	"apiKey" : 'AIzaSyAFAM421tHTh5AWZzXvp8WBTF3TeJIaWe4',
+	"authDomain" : "my-awesome-project-13077.firebaseapp.com",
+	"databaseURL" : "https://my-awesome-project-13077.firebaseio.com/",
+	"storageBucket" : "my-awesome-project-13077.appspot.com"
+}
+
 
 app = Flask(__name__)
-firebase = firebase.FirebaseApplication('https://my-awesome-project-13077.firebaseio.com/', None)
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
 @app.route("/")
 def hello():
@@ -11,13 +21,24 @@ def hello():
 @app.route("/signup", methods=["GET", "POST"])
 def sign_up():
 	if request.method == 'GET':
-		return "this is a get method"
-	return "ok"
-	# else:
-		# name = request.form["name"]
-		# result = firebase.put('/users', 'name', name)
-		# return name
-	# return "ok" 
+		return "render signup template"
+	else:
+		email = request.form["email"]
+		password = request.form["password"]
+		auth.create_user_with_email_and_password(email, password)
+		user =  auth.sign_in_with_email_and_password(email, password)
+		return user
+	return "ok" 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	if request.method == 'GET':
+		email = request.args.get('email')
+		print("HELLO" + email)
+		password = request.args.get('password')
+		print("BYE " + password)
+		user =  auth.sign_in_with_email_and_password(email, password)
+		return user
 
 # TODO authentication check before changing database
 
